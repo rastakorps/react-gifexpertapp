3e5c79e4bf9c739192d764286bd04bc6dc847f17
Jose Cruz <jose.oakenfold@gmail.com>
2020-08-03 23:30:31 -0500

    Docs agregado

---
+++ docs/precache-manifest.619a648d05dfe2994bb1a9966a1633ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b138ff0e4f1dff131f2807ba2b9c6c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8216ed3a3ea778230836",
+    "url": "/static/css/main.0f5c7bb7.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8216ed3a3ea778230836",
+    "url": "/static/js/main.fac5546e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
